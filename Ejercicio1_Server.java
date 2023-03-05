ff497a8f5f2e7e4b746b31747ed812a9f555185d
rodri0319 <57386040+rodri0319@users.noreply.github.com>
2023-03-04 23:41:44 -0600

    Update Ejercicio1_Server.java

--- Ejercicio1_Server.java
+++ Ejercicio1_Server.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class Server {
     public static void main(String[] args) throws IOException {
-        
+
         ServerSocket server = null;
 
         try {
@@ -18,7 +18,7 @@ public class Server {
 
         try {
             cliente = server.accept();
-            System.out.println("Cliente conectado: "+cliente.getInetAddress());
+            System.out.println("Cliente conectado: " + cliente.getInetAddress());
         } catch (IOException e) {
             System.err.println("Fallo al aceptar la petición");
             System.exit(1);
@@ -29,17 +29,58 @@ public class Server {
 
         String LineaEntrada, LineaSalida;
 
-        while((LineaEntrada = LectorEntrada.readLine()) != null) {
-            System.out.println("Cliente: "+LineaEntrada);
-            LineaSalida = LineaEntrada;
+        ClientHandler ClientHandler = new ClientHandler();
 
-            EscritorSalida.println("Hola Bienvenido al server");
-            if(LineaSalida.equals("Bye."))
-            break;
+        while ((LineaEntrada = LectorEntrada.readLine()) != null) {
+            if (LineaEntrada.equalsIgnoreCase("Adios")) {
+                EscritorSalida.println("Adios");
+                break;
+            } else {
+                System.out.println("Cliente: " + LineaEntrada);
+                LineaSalida = ClientHandler.processInput(LineaEntrada);
+                EscritorSalida.println(LineaSalida);
+            }
         }
         EscritorSalida.close();
         LectorEntrada.close();
         cliente.close();
         server.close();
+    }
+}
+
+class ClientHandler {
+    private static final int ESPERANDO = 0;
+    private static final int ENVIARESP1 = 1;
+    private static final int ENVIARESP2 = 2;
+    private static final int OTRA = 3;
+
+    private int estado;// = ESPERANDO;
+
+    private String[] resp1 = { "Hola, bienvenido al server", "Mi nombre es Servidor", "Server dice hola" };
+
+    public String processInput(String TextoRecibido) {
+        String textoDeSalida = null;
+        if (estado == ESPERANDO) {
+            textoDeSalida = "Esperando cadena de texto";
+            estado = ENVIARESP1;
+        } else if (estado == ENVIARESP1) {
+            if (TextoRecibido.equalsIgnoreCase("Hola")) {
+                textoDeSalida = resp1[2];
+                estado = ENVIARESP2;
+            } else {
+                textoDeSalida = "No reconozco el mensaje, intenta mandando Hola";
+            }
+        } else if (estado == ENVIARESP2) {
+            if (TextoRecibido.equalsIgnoreCase("Nombre")) {
+                textoDeSalida = resp1[1];
+                estado = OTRA;
+            } else {
+                textoDeSalida = "Prueba escribiendo Nombre\n";
+                estado = ENVIARESP1;
+            }
+        } else if (estado == OTRA) {
+            textoDeSalida = "Adios";
+        }
+        return textoDeSalida;
     }
 }
