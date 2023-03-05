a05ac81ba420b84b97e7341787e34e243fd610b6
rodri0319 <57386040+rodri0319@users.noreply.github.com>
2023-03-04 23:40:49 -0600

    Update Ejercicio1_Cliente.c

--- Ejercicio1_Cliente.c
+++ Ejercicio1_Cliente.c
@@ -13,31 +13,35 @@ int main()
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(1234);
     server_addr.sin_addr.s_addr = INADDR_ANY;
-    // inet_aton("192.168.0.1", &server_addr.sin_addr);
 
     connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr));
-    printf("ConexiÃ³n establecida\n");
+    printf("Conexión establecida\n");
     char message[100], server_resp[100];
 
     while (1)
     {
+        char message[100] = {0};
+        char server_resp[100] = {0};
         printf("Ingrese el mensaje: ");
         fgets(message, 100, stdin);
+        fflush(stdin);
 
-        if(send(sock, message, strlen(message), 0)<0)
+        if (send(sock, message, strlen(message), 0) < 0)
         {
             printf("Error al enviar\n");
             exit(1);
         }
 
-        if(recv(sock, server_resp, 100, 0)<0)
+        if (recv(sock, server_resp, 100, 0) < 0)
         {
             printf("Error al recibir\n");
             exit(1);
         }
         printf("Respuesta del servidor: %s\n", server_resp);
-        if (strcmp(server_resp, "Bye.") == 0)
-            break;
+        if (strcmp(message, "Adios") == 0)
+        {
+            close(sock);
+        }
     }
     close(sock);
     return 0;
